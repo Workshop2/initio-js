9ec60c416ca61867de70ad3eac4f31a77abb4357
Simon Colmer <simon_colmer@hotmail.com>
2015-08-09 19:15:32 +0100

    Added some comments

--- initio.js
+++ initio.js
@@ -4,7 +4,14 @@ var board = new five.Board({
   io: new Raspi()
 });
 
+console.log("Hello :)")
+
 board.on("ready", function() {
+  console.log("ready")
   var led = new five.Led("P1-13");
   led.blink();
+    console.log("done ready")
 });
+
+
+console.log("Something else?")
