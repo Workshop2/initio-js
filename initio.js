b7f1ccb281dec9457cee9a3de42bca657ea4ed29
Simon Colmer <simon_colmer@hotmail.com>
2015-08-09 19:39:22 +0100

    Wondering what will happen

--- initio.js
+++ initio.js
@@ -4,14 +4,20 @@ var board = new five.Board({
   io: new Raspi()
 });
 
+
+var R1 = "GPIO24";
+var R2 = "GPIO26";
+var L1 = "GPIO19";
+var L2 = "GPIO21";
+
+
 console.log("Hello :)")
 
 board.on("ready", function() {
   console.log("ready")
-  var led = new five.Led("P1-13");
-  led.blink();
-    console.log("done ready")
+
+  var frontLeftMotor = new five.Motor(L1);
+  console.log(frontLeftMotor);
+//  var led = new five.Led("P1-13");
+//  led.blink();
 });
-
-
-console.log("Something else?")
